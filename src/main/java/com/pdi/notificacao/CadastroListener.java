75225fbab23a22363a2f1533c52439cbebab0938
Geanderson Oliveira <gean.motog@gmail.com>
2023-11-27 02:14:20 -0300

    First commit

---
+++ src/main/java/com/pdi/notificacao/CadastroListener.java
@@ -0,0 +1,16 @@
+package com.pdi.notificacao;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CadastroListener {
+	
+	@RabbitListener (queues = "queue.v1.cadastro-criado")
+	public void onCadastroCreated (dtoCadastro mensagem) {
+		
+		System.out.println("Novo Cadastro Realizado com Id " + mensagem.getId());
+		System.out.println("Nome do Cliente " + mensagem.getName());
+		
+	}
+}
